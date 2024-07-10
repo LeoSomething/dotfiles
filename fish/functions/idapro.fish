c72f8cdfd45f3e20f04c6557c779fddde956af44
leo_something <leonardomazzorana@protonmail.com>
2024-07-10 20:12:08 +0200

    first commit

---
+++ fish/functions/idapro.fish
@@ -0,0 +1,4 @@
+function idapro --wraps=wine\ /home/leo/.apps/IDA\\\ 7.5/ida.exe --description alias\ idapro\ wine\ /home/leo/.apps/IDA\\\ 7.5/ida.exe
+  wine /home/leo/.apps/IDA\ 7.5/ida.exe $argv
+        
+end
---
+++ fish/functions/idapro64.fish
@@ -0,0 +1,4 @@
+function idapro64 --wraps=wine\ /home/leo/.apps/IDA\\\ 7.5/ida64.exe --description alias\ idapro64\ wine\ /home/leo/.apps/IDA\\\ 7.5/ida64.exe
+  wine /home/leo/.apps/IDA\ 7.5/ida64.exe $argv
+        
+end
